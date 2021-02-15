646ed7d17e982c1aa7db296ce85f92f079f910a7
Maurice Putz <mauriceputz2000@gmail.com>
2021-02-15 13:07:43 +0100

    +adding code highlight in project +fixing meson build options

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file boost_license.txt)
 
+#include "CLI11.hpp"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/async.h"
+
 using namespace std;
 
 int main() {
