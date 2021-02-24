a65bc0dced3b375efcdc08e77c50ede5133da924
Maurice Putz <mauriceputz2000@gmail.com>
2021-02-24 20:19:37 +0100

    +fixing meson build

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file boost_license.txt)
 
+//#pragma GCC diagnostic push
+//#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+//#pragma GCC diagnostic pop
 #include "CLI11.hpp"
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/async.h"
+#include "asio.hpp"
 
 using namespace std;
 
