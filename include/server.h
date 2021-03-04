9ef5b29e8ef03802472af61cb97aa6cbdec1017c
Maurice Putz <mauriceputz2000@gmail.com>
2021-03-04 23:01:43 +0100

    +updating meson build +adding changelog file

--- include/server.h
+++ include/server.h
@@ -1,3 +1,7 @@
+//          Copyright Maurice Putz 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file boost_license.txt)
+
 #pragma once
 
 #include <iostream>
